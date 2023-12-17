8a1c7f92f35e6d1fea6e278a124bc7e883f0266f
Thành Phan <14072k7@gmail.com>
2023-12-17 11:16:28 +0700

    /

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ import turtle as t
 import time
 screen = t.Screen()
 screen.setup(640,360)
-screen.bgpic("./background.gif")
+screen.bgpic("picture\\background\\background.gif")
 screen.update()
-screen.addshape('C:\\Users\\NCPC\\Documents\\New folder\\hc phần 2\\DỰ ÁN\\ship.gif')
-t.shape("C:\\Users\\NCPC\\Documents\\New folder\\hc phần 2\\DỰ ÁN\\ship.gif")
+screen.addshape('picture\\character\\ship.gif')
+t.shape("picture\\character\\ship.gif")
 screen.exitonclick()
 # 
 screen.listen()
-
