3fa54121e175743839fd7ac40531f8411eb52f42
Thành Phan <14072k7@gmail.com>
2023-12-17 11:08:27 +0700

    thành

---
+++ main.py
@@ -0,0 +1,12 @@
+import turtle as t
+import time
+screen = t.Screen()
+screen.setup(640,360)
+screen.bgpic("./background.gif")
+screen.update()
+screen.addshape('C:\\Users\\NCPC\\Documents\\New folder\\hc phần 2\\DỰ ÁN\\ship.gif')
+t.shape("C:\\Users\\NCPC\\Documents\\New folder\\hc phần 2\\DỰ ÁN\\ship.gif")
+screen.exitonclick()
+# 
+screen.listen()
+
